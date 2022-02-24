4cd9ba3d8bd63aba085f882ffa0eb4aecd6a1b3f
“saimanikant” <saimanikant@gmail.com>
2022-02-24 15:07:55 +0000

    End of Chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -4,5 +4,12 @@ from django.contrib import admin
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('name',)}
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
--- rango/urls.py
+++ rango/urls.py
@@ -6,4 +6,6 @@ app_name='rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+    views.show_category, name='show_category'),
 ]
