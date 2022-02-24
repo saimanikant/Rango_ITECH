4041d87990ca550f25d6582d0a3bd116525d14dc
“saimanikant” <saimanikant@gmail.com>
2022-02-24 15:03:49 +0000

    Chapter 6.2

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
+
 
 def index(request): 
     # Query the database for a list of ALL categories currently stored. 
@@ -20,6 +22,22 @@ def index(request):
 
 def about(request):
     about_context_dict = {'boldmessage': "Rango says here is the about page."}
-    return render(request, 'rango/about.html', context=about_context_dict)
+    return render(request, 'rango/about.html', about_context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        page = Page.objects.filter(category=category)
+
+        context_dict['pages'] = page
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', context_dict)
 
 
